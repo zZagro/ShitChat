09d77be89834f6ca913fc5b20b9a48fa8ca2251f
zZagro <samuel.nahm1@gmail.com>
2023-04-27 22:21:12 +0200

    Navbar items are b*llsh*t

---
+++ app/src/main/java/de/zagro/shitchat/ui/direct/DirectFragment.java
@@ -0,0 +1,36 @@
+package de.zagro.shitchat.ui.direct;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import de.zagro.shitchat.databinding.FragmentDirectBinding;
+import de.zagro.shitchat.ui.home.HomeViewModel;
+
+public class DirectFragment extends Fragment {
+
+    private FragmentDirectBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        DirectViewModel directViewModel = new ViewModelProvider(this).get(DirectViewModel.class);
+
+        binding = FragmentDirectBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDirect;
+        directViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/de/zagro/shitchat/ui/direct/DirectViewModel.java
@@ -0,0 +1,18 @@
+package de.zagro.shitchat.ui.direct;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DirectViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DirectViewModel()
+    {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is direct fragment!");
+    }
+
+    public LiveData<String> getText() { return mText; }
+}
