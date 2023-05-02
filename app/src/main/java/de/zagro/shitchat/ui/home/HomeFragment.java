2cdce70acd75cf25a1414adabe47c84fdff52ea2
zZagro <samuel.nahm1@gmail.com>
2023-05-02 20:58:38 +0200

    changed home design

--- app/src/main/java/de/zagro/shitchat/ui/home/HomeFragment.java
+++ app/src/main/java/de/zagro/shitchat/ui/home/HomeFragment.java
@@ -24,13 +24,10 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         return root;
     }
